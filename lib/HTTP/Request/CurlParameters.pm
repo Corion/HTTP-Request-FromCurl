8681a1238c29c46414566667d57b8cb2ead670af
Max Maischein <corion@corion.net>
2018-11-14 18:55:09 +0100

    Don't escape the double quote in the generated code

--- lib/HTTP/Request/CurlParameters.pm
+++ lib/HTTP/Request/CurlParameters.pm
@@ -72,7 +72,7 @@ has output => (
 
 sub _build_body( $self ) {
     if( my $body = $self->body ) {
-        $body =~ s!([\x00-\x1f"'\\])!sprintf '\\x%02x', ord $1!ge;
+        $body =~ s!([\x00-\x1f'\\])!sprintf '\\x%02x', ord $1!ge;
         return sprintf "'%s'", $body
 
     } else {
