79c59d676655d8b51422f61522428f75e456b3bb
Max Maischein <corion@corion.net>
2018-11-15 18:11:51 +0100

    Add automatic HTML::Tidy support

--- script/curl2lwp.pl
+++ script/curl2lwp.pl
@@ -4,14 +4,23 @@ use warnings;
 
 use HTTP::Request::FromCurl;
 
+my $has_tidy;
+BEGIN { eval { require Perl::Tidy; $has_tidy = 1; } }
+
 my $request = HTTP::Request::FromCurl->new(
     argv => [ @ARGV ],
     read_files => 1,
-);
+) or exit 1; # Getopt::Long has already printed the error message
 
-if( $request ) {
-    print $request->as_snippet( type => 'LWP' );
-} else {
-    # Getopt::Long has already printed the error message
-    exit 1;
-};
+my $lwp = $request->as_snippet( type => 'LWP' );
+
+if( $has_tidy and ! $no_tidy) {
+    my $formatted;
+    Perl::Tidy::perltidy(
+        source      => \$lwp,
+        destination => \$formatted,
+        argv        => [ '--no-memoize' ],
+    ) or $lwp = $formatted;
+}
+
+print $lwp;
