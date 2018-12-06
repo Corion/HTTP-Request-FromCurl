9d66682bf7ee58e1aa8f2d792c241d601009081a
Max Maischein <corion@corion.net>
2018-12-06 21:07:10 +0100

    Bump version

--- lib/HTTP/Request/CurlParameters.pm
+++ lib/HTTP/Request/CurlParameters.pm
@@ -13,7 +13,7 @@ use Filter::signatures;
 use feature 'signatures';
 no warnings 'experimental::signatures';
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 =head1 NAME
 
--- lib/HTTP/Request/FromCurl.pm
+++ lib/HTTP/Request/FromCurl.pm
@@ -14,7 +14,7 @@ use Filter::signatures;
 use feature 'signatures';
 no warnings 'experimental::signatures';
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 =head1 NAME
 
