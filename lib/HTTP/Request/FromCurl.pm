f20349e103a402f41b6d30ec7e62589312b62332
Max Maischein <corion@corion.net>
2020-04-10 12:44:03 +0200

    Update Changes, bump version

--- lib/HTTP/Request/FromCurl.pm
+++ lib/HTTP/Request/FromCurl.pm
@@ -15,7 +15,7 @@ use Filter::signatures;
 use feature 'signatures';
 no warnings 'experimental::signatures';
 
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 =head1 NAME
 
--- script/pcurl.pl
+++ script/pcurl.pl
@@ -6,7 +6,7 @@ use HTTP::Request::FromCurl;
 use LWP::UserAgent;
 use Getopt::Long ':config','pass_through';
 
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 # parse output options from @ARGV
 GetOptions(
