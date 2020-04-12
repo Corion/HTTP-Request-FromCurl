c4f8aa6fb2a5380252206d7e72d5f2cd47191f12
Max Maischein <corion@corion.net>
2020-04-12 17:04:57 +0200

    Add release date, bump version, regenerate README.*

--- lib/HTTP/Request/FromFetch.pm
+++ lib/HTTP/Request/FromFetch.pm
@@ -9,7 +9,7 @@ use JSON;
 use PerlX::Maybe;
 use HTTP::Request::CurlParameters;
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 
 =head1 NAME
 
--- t/TestCurlIdentity.pm
+++ t/TestCurlIdentity.pm
@@ -18,7 +18,7 @@ no warnings 'experimental::signatures';
 use Exporter 'import';
 
 our @EXPORT_OK = (qw(&run_curl_tests $server));
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 
 $Data::Dumper::Useqq = 1;
 
