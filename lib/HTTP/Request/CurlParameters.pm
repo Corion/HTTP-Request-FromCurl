0bc3fbc700f6faa8d7cd8c489b5b6cb2a7bd360c
Max Maischein <corion@corion.net>
2021-10-17 12:11:11 +0200

    Bump version, update Changes, update META.*

--- lib/HTTP/Request/CurlParameters.pm
+++ lib/HTTP/Request/CurlParameters.pm
@@ -14,7 +14,7 @@ use Filter::signatures;
 use feature 'signatures';
 no warnings 'experimental::signatures';
 
-our $VERSION = '0.27';
+our $VERSION = '0.28';
 
 =head1 NAME
 
--- lib/HTTP/Request/FromFetch.pm
+++ lib/HTTP/Request/FromFetch.pm
@@ -9,7 +9,7 @@ use JSON;
 use PerlX::Maybe;
 use HTTP::Request::CurlParameters;
 
-our $VERSION = '0.27';
+our $VERSION = '0.28';
 
 =head1 NAME
 
