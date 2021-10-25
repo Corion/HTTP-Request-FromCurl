e48926e1ef6c51446864770202557015132603f8
Max Maischein <corion@corion.net>
2021-10-25 23:01:13 +0200

    Bump version, Update Changes

--- script/pwget.pl
+++ script/pwget.pl
@@ -6,7 +6,7 @@ use HTTP::Request::FromWget;
 use LWP::UserAgent;
 use Getopt::Long ':config','pass_through';
 
-our $VERSION = '0.34';
+our $VERSION = '0.35';
 
 # parse output options from @ARGV
 GetOptions(
