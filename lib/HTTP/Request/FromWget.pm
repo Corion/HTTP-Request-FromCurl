79d3efc882378a1c2ea18fc4a01fda737a54023d
Max Maischein <corion@corion.net>
2023-04-14 20:55:16 +0200

    Bump copyright

--- lib/HTTP/Request/FromWget.pm
+++ lib/HTTP/Request/FromWget.pm
@@ -530,7 +530,7 @@ Max Maischein C<corion@cpan.org>
 
 =head1 COPYRIGHT (c)
 
-Copyright 2018-2022 by Max Maischein C<corion@cpan.org>.
+Copyright 2018-2023 by Max Maischein C<corion@cpan.org>.
 
 =head1 LICENSE
 
