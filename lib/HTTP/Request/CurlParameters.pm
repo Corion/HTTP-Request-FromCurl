a31f8b42a3b98805844cbdf2073d1e905551d62f
Max Maischein <corion@corion.net>
2025-02-01 08:03:57 +0100

    LWP::UserAgent uses SSL_opts , not SSL_options for SSL options
    Diagnosed and submitted by Mark West

--- lib/HTTP/Request/CurlParameters.pm
+++ lib/HTTP/Request/CurlParameters.pm
@@ -616,7 +616,7 @@ sub as_lwp_snippet( $self, %options ) {
                                maybe max_size      => $self->max_filesize,
                                maybe timeout       => $self->timeout,
                                maybe cookie_jar    => $init_cookie_jar->{code},
-                               maybe SSL_options   => keys %ssl_options ? \%ssl_options : undef,
+                               maybe SSL_opts      => keys %ssl_options ? \%ssl_options : undef,
                            ], '')
                            ;
     if( defined( my $credentials = $self->credentials )) {
@@ -708,7 +708,7 @@ sub as_http_tiny_snippet( $self, %options ) {
     };
     if( $self->cert ) {
         push @preamble, 'use IO::Socket::SSL;';
-        $ssl_options{ SSL_ca_file } = $self->cert;
+        $ssl_options{ SSL_cert_file } = $self->cert;
     };
     if( $self->show_error ) {
         push @postamble,
